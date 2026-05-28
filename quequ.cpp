4868eb260d05913325a16c598a2b695161766acc
Hirzee <faruqhirzi698@gmail.com>
2026-05-28 10:33:27 +0700

    mendeteksi error

--- quequ.cpp
+++ quequ.cpp
@@ -131,12 +131,4 @@ int main()
     while (true)
     {
         try
-        {
-            cout << "Menu" << endl;
-            cout << "1. Implement Insert operasi" << endl;
-            cout << "2. Implement Delete Operasi" << endl;
-            cout << "3. Display values" << endl;
-            cout << "4. Exit " << endl;
-            cout << "Enter your choice(1-4): ";
-            cin >> ch;
-            cout << endl;
+     
